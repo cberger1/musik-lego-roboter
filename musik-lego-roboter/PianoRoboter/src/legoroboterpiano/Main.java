45db2d70f2474dac7b30e1550bf656ce53316151
cberger1 <b5.xb360@gmail.com>
2017-04-05 15:00:48 +0200

    Add files via upload
    Communication

---
+++ musik-lego-roboter/PianoRoboter/src/legoroboterpiano/Main.java
@@ -0,0 +1,17 @@
+package legoroboterpiano;
+
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lieder.Allmyducks();
+	}
+	
+}
+
+
