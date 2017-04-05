607837abcff88e57e3c3d171910a29767e873f7f
Joel <Joel@Joels-PC.fritz.box>
2017-04-05 16:00:08 +0200

    Überflüssige Funktionen gelöscht

--- PianoRoboter/src/legoroboterpiano/Lieder.java
+++ PianoRoboter/src/legoroboterpiano/Lieder.java
@@ -5,31 +5,8 @@ package legoroboterpiano;
 //import motorschnelligkeit.StopWatch;
 
 public class Lieder {
-	// static int t = 0;
-	// public static long mosp;
-	//
-	// public static long motorspeed(int umdrehungen){
-	// // Wie lange braucht der LegoRoboter um die KeyBoard Taste zu drücken
-	// StopWatch s = new StopWatch();
-	// long total = 0;
-	// for (int i = 0; i<umdrehungen ; i++){
-	// s.start();
-	// Motor.C.rotate(45);
-	// Motor.C.rotate(-45);
-	// s.stop();
-	// long t = s.getElapsedTime();
-	// total = total + t;
-	//
-	// }
-	// total=total/umdrehungen;
-	// return total;
-	//
-	// // Ziemlich genau 700ms (mit fast leeren Batterien
-	// // nochmals mit zu 1/2 gefüllte Batterien getestet immer 568ms
-	// }
-
 	static Funktionen f = new Funktionen();
-	
+
 	public static void Allmyducks(int t) {
 		Funktionen.mosp = f.motorspeed(5);
 		f.spielen("c", 50);
@@ -60,40 +37,5 @@ public class Lieder {
 		f.spielen("g", 50);
 		f.spielen("c", 100);
 	}
-	// public static void spielen(String n, int x ){
-	// int drehen = 45;
-	// int tempo = t;
-	// int d = (int) ((x*600/tempo)- mosp);
-	// if(n.equals("c")){
-	// Motor.A.rotate(drehen);
-	// Delay.msDelay(d);
-	// Motor.A.rotate(-drehen);
-	// }
-	// if(n.equals("d")){
-	// Motor.A.rotate(-drehen);
-	// Delay.msDelay(d);
-	// Motor.A.rotate(drehen);
-	// }
-	// if(n.equals("e")){
-	// Motor.B.rotate(drehen);
-	// Delay.msDelay(d);
-	// Motor.B.rotate(-drehen);
-	// }
-	// if(n.equals("f")){
-	// Motor.B.rotate(-drehen);
-	// Delay.msDelay(d);
-	// Motor.B.rotate(drehen);
-	// }
-	// if(n.equals("g")){
-	// Motor.C.rotate(drehen);
-	// Delay.msDelay(d);
-	// Motor.C.rotate(-drehen);
-	// }
-	// if(n.equals("a")){
-	// Motor.C.rotate(-drehen);
-	// Delay.msDelay(d);
-	// Motor.C.rotate(drehen);
-	// }
-	// }
 
 }
