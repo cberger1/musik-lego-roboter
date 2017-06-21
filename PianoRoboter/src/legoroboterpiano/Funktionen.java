8d462574fc7f662eb213f6d4cd60b57109159107
cberger1 <b5.xb360@gmail.com>
2017-06-21 15:41:52 +0200

    Update Funktionen.java

--- PianoRoboter/src/legoroboterpiano/Funktionen.java
+++ PianoRoboter/src/legoroboterpiano/Funktionen.java
@@ -12,36 +12,56 @@ import lejos.util.Delay;
 import motorschnelligkeit.StopWatch;
 
 public class Funktionen {
-	static int t = 0;
-	public static long mosp;
-	public static RemoteNXT nxt;
 	
-	public long motorspeed(int umdrehungen) {
-		// Wie lange braucht der LegoRoboter um die KeyBoard Taste zu drücken
+	public static int r = 50;
+	public static int t = 100;
+	public static long mosp;
+	public static long kal;
+	public static RemoteNXT nxt;
+	public static int drehen = 22;
+	
+	public long motorspeed(int umdrehungen){
 		StopWatch s = new StopWatch();
 		long total = 0;
-		Motor.C.setSpeed(1000);
-		for (int i = 0; i < umdrehungen; i++) {
+		Motor.A.setSpeed(1000);
+		for (int i = 0; i<umdrehungen ; i++){
 			s.start();
-			Motor.C.rotate(45);
-			Motor.C.rotate(-45);
+			Motor.A.setSpeed(1000);
+			Motor.A.rotate(drehen);
+			Motor.A.rotate(-drehen);
 			s.stop();
 			long t = s.getElapsedTime();
 			total = total + t;
-
 		}
-		total = total / umdrehungen;
+		total=total/umdrehungen;
+		LCD.clear();
+		LCD.drawString("mosp1 = " + total, 0, 0);
 		return total;
-
-		// Ziemlich genau 700ms (mit fast leeren Batterien
-		// nochmals mit zu 1/2 gefüllte Batterien getestet immer 568ms
+	}
+	public long kalibrieren(int umdrehungen){
+		StopWatch s = new StopWatch();
+		long total = 0;
+		nxt.A.setSpeed(1000);
+		for (int i = 0; i<umdrehungen ; i++){
+			s.start();
+			nxt.A.setSpeed(1000);
+			nxt.A.rotate(-drehen);
+			nxt.A.rotate(drehen);
+			s.stop();
+			long t = s.getElapsedTime();
+			total = total + t;
+		}
+		total=total/umdrehungen;
+		LCD.drawString("mosp2 = " + total, 0, 1);
+		return total;
 	}
 	public static void connecting(){
 		try {
 			LCD.drawString("Connecting...", 0, 0);
 			NXTCommConnector connector = Bluetooth.getConnector();
-			nxt = new RemoteNXT("NXT", connector);
-			
+			nxt = new RemoteNXT("Colin", connector);
+			LCD.clear();
+
 		} catch (IOException ioe) {
 			LCD.clear();
 			LCD.drawString("Conn Failed", 0, 0);
@@ -49,77 +69,77 @@ public class Funktionen {
 			System.exit(1);
 		}
 	}
-	
-	
-
 	public void pl(String n, int x) {
-		int drehen = 45;
 		int tempo = t;
 		Motor.A.setSpeed(1000);
 		Motor.B.setSpeed(1000);
 		Motor.C.setSpeed(1000);
+		nxt.A.setSpeed(1000);
+		nxt.B.setSpeed(1000);
+		nxt.C.setSpeed(1000);
+
+		int d1 = (int) ((x * 600 / tempo) - mosp);
+		int d2 = (int) (((x * 600 / tempo) - kal)/2);
 		
-		int d = (int) ((x * 600 / tempo) - mosp);
 		if (n.equals("c")) {
-			Motor.A.rotate(drehen);
-			Delay.msDelay(d);
-			Motor.A.rotate(-drehen);
+			nxt.A.rotate(-drehen);
+			Delay.msDelay(d2);
+			nxt.A.rotate(drehen);
 		}
 		if (n.equals("d")) {
-			Motor.A.rotate(-drehen);
-			Delay.msDelay(d);
-			Motor.A.rotate(drehen);
+			nxt.A.rotate(drehen);
+			Delay.msDelay(d2);
+			nxt.A.rotate(-drehen);
 		}
 		if (n.equals("e")) {
-			Motor.B.rotate(drehen);
-			Delay.msDelay(d);
-			Motor.B.rotate(-drehen);
+			nxt.B.rotate(-drehen);
+			Delay.msDelay(d2);
+			nxt.B.rotate(drehen);
 		}
 		if (n.equals("f")) {
-			Motor.B.rotate(-drehen);
-			Delay.msDelay(d);
-			Motor.B.rotate(drehen);
+			nxt.B.rotate(drehen);
+			Delay.msDelay(d2);
+			nxt.B.rotate(-drehen);
 		}
 		if (n.equals("g")) {
-			Motor.C.rotate(drehen);
-			Delay.msDelay(d);
-			Motor.C.rotate(-drehen);
+			nxt.C.rotate(-drehen);
+			Delay.msDelay(d2);
+			nxt.C.rotate(drehen);
 		}
 		if (n.equals("a")) {
-			Motor.C.rotate(-drehen);
-			Delay.msDelay(d);
-			Motor.C.rotate(drehen);
+			nxt.C.rotate(drehen);
+			Delay.msDelay(d2);
+			nxt.C.rotate(-drehen);
 		}
 		if (n.equals("h")) {
-			nxt.A.rotate(-drehen);
-			Delay.msDelay(d);
-			nxt.A.rotate(drehen);
+			Motor.A.rotate(drehen);
+			Delay.msDelay(d1);
+			Motor.A.rotate(-drehen);
 		}
 		if (n.equals("C")) {
 			Motor.A.rotate(-drehen);
-			Delay.msDelay(d);
+			Delay.msDelay(d1);
 			Motor.A.rotate(drehen);
 		}
 		if (n.equals("D")) {
 			Motor.B.rotate(drehen);
-			Delay.msDelay(d);
+			Delay.msDelay(d1);
 			Motor.B.rotate(-drehen);
 		}
 		if (n.equals("E")) {
 			Motor.B.rotate(-drehen);
-			Delay.msDelay(d);
+			Delay.msDelay(d1);
 			Motor.B.rotate(drehen);
 		}
 		if (n.equals("F")) {
-			Motor.C.rotate(drehen);
-			Delay.msDelay(d);
 			Motor.C.rotate(-drehen);
+			Delay.msDelay(d1);
+			Motor.C.rotate(drehen);
 		}
 		if (n.equals("G")) {
-			Motor.C.rotate(-drehen);
-			Delay.msDelay(d);
 			Motor.C.rotate(drehen);
+			Delay.msDelay(d1);
+			Motor.C.rotate(-drehen);
 		}
 	}
-
 }
