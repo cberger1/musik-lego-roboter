2af328fc5b85e6a5afe576236cd6b080a92d5af0
cberger1 <b5.xb360@gmail.com>
2017-06-21 15:39:45 +0200

    Update Main.java

--- PianoRoboter/src/legoroboterpiano/Main.java
+++ PianoRoboter/src/legoroboterpiano/Main.java
@@ -1,18 +1,28 @@
 package legoroboterpiano;
 
+import lejos.nxt.Button;
+
+
 
 public class Main {
-
-	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Lieder.Allmyducks(100);
+		Lieder.Test();
+		
+		Lieder.Allmyducks();
+		
+		Lieder.SongOfTime();
+		
+		Button.waitForAnyPress();
+		
+		Lieder.StarWarsTheme();
 	}
 
 }
+
 
 
